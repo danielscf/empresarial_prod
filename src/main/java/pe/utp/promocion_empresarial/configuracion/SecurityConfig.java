4bfffea720724ec972ab9094147d5dee2b1a5dbd
danielscf <fdanielsantacruz@gmail.com>
2024-11-22 18:48:26 -0500

    Cors

--- src/main/java/pe/utp/promocion_empresarial/configuracion/SecurityConfig.java
+++ src/main/java/pe/utp/promocion_empresarial/configuracion/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -52,15 +53,15 @@ public class SecurityConfig implements WebMvcConfigurer {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(request -> request
                         //.requestMatchers("/login", "/solicitud/emprendedor/usuario").permitAll()
                         .anyRequest().permitAll())
-                .httpBasic(httpBasic -> httpBasic.disable())  // Desactiva la autenticación básica
+                .httpBasic(AbstractHttpConfigurer::disable)  // Desactiva la autenticación básica
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
